167cb5e22b0b080510dcf1265ed453f9b4140d5f
AtharvGaur <atharava112@gmail.com>
2025-04-07 21:54:21 +0530

    Implement Clerk authentication and theme management; refactor layout and home components

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import { SignInButton } from '@clerk/nextjs'
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <SignInButton/>
+    </div>
+  )
+}
+
+export default Header
