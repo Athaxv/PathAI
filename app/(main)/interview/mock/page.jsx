c55d18bc720900f8d1d0b83a68d46ed8cf261c10
AtharvGaur <atharava112@gmail.com>
2025-04-18 12:37:24 +0530

    feat: add RadioGroup and RadioGroupItem components; update package dependencies

--- app/(main)/interview/mock/page.jsx
+++ app/(main)/interview/mock/page.jsx
@@ -6,7 +6,7 @@ import Quiz from '../_components/Quiz'
 
 function Page() {
   return (
-    <div className='container mx-auto space-y-4 py-6'>
+    <div className='container mx-auto space-y-4 py-3'>
       <div className='flex flex-col space-y-2 mx-2'>
         <Link href={"/interview"}>
           <Button variant={"link"} className={"gap-2 pl-0"}>
