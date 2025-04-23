be6e27c61fbe341212601a2cff88f0b470b53f54
AtharvGaur <atharava112@gmail.com>
2025-04-24 00:09:59 +0530

    auth fixed

--- actions/CoverLetter.js
+++ actions/CoverLetter.js
@@ -1,13 +1,22 @@
 "use server"
 import { auth } from "@clerk/nextjs";
-
+import { db } from "@/lib/prisma"
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
 })
 
 export async function getCoverLetter() {
-    const { userId } = auth();
-    if (!userId) throw new Error("Unauthorized")
-       
+    const { userId } = await auth()
+        if (!userId) throw new Error("Unauthorized")
+    
+        const user = await db.user.findUnique({
+            where: {
+                clerkUserId: userId
+            },
+        })
+
+    if (!user) throw new Error("User not Found")
+
+    
 }
