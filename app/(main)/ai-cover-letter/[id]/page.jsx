4692db0db1418c561460a3dc5efdfe2cca77b4d9
AtharvGaur <atharava112@gmail.com>
2025-04-26 22:22:04 +0530

    feat: enhance CoverLetter page with dynamic content and add CoverLetterPreview component

--- app/(main)/ai-cover-letter/[id]/page.jsx
+++ app/(main)/ai-cover-letter/[id]/page.jsx
@@ -1,9 +1,30 @@
+import { getCoverLetter } from '@/actions/CoverLetter';
+import { Button } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react'
+import CoverLetterPreview from '../_components/CoverLetterPreview';
 
 async function CoverLetter({ params }) {
     const {id} = await params;
+    const coverLetter = await getCoverLetter(id);
+
   return (
-    <div>CoverLetter: {id}</div>
+    <div className='mx-auto container py-6'>
+      <div className='flex flex-col space-y-2'>
+        <Link href={'/ai-cover-letter'}>
+          <Button variant={'link'} className={'gap-2 pl-0'}>
+            <ArrowLeft className='h-4 w-4'/>
+            Back to Cover Letters
+          </Button>
+        </Link>
+
+        <h1 className='text-6xl font-bold gradient-title mb-6'>
+          {coverLetter?.jobTitle} at {coverLetter?.companyName}
+        </h1>
+      </div>
+      <CoverLetterPreview/>
+    </div>
   )
 }
 
---
+++ app/(main)/ai-cover-letter/_components/CoverLetterPreview.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CoverLetterPreview() {
+  return (
+    <div>CoverLetterPreview</div>
+  )
+}
+
+export default CoverLetterPreview
